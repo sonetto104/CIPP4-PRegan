0bd8101d70ccba3b033e686ede068175568c1e31
sonetto104 <peter_regan88@yahoo.com>
2023-06-22 22:04:10 +0000

    Edit settings.py. Add textpost and textcomment model and migrate to database.

--- pianophiles/settings.py
+++ pianophiles/settings.py
@@ -131,7 +131,8 @@ SATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-MEDIA_URL = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+MEDIA_URL = '/media/'
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
