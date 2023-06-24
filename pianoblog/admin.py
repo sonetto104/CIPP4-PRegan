d7741411bf006f4ef58b57bbe3fe6a068a29e3dd
sonetto104 <peter_regan88@yahoo.com>
2023-06-24 17:28:37 +0000

    Add comment approval to admin site.

--- pianoblog/admin.py
+++ pianoblog/admin.py
@@ -1,10 +1,27 @@
 from django.contrib import admin
-from .models import TextPost
+from .models import TextPost, TextComment
 from django_summernote.admin import SummernoteModelAdmin
+
+# admin.register block taken from Code Institute's Codestar Walkthrough Project
+
 
 @admin.register(TextPost)
 class TextPostAdmin(SummernoteModelAdmin):
     
+    list_display = ('title', 'slug', 'status', 'created_on')
     prepopulated_fields = {"slug": ("title",)}
     list_filter = ("status", "created_on")
+    search_fields = ['title', 'content']
     summernote_fields = ('content')
+
+# admin.register block taken from Code Institute's Codestar Walkthrough Project
+
+@admin.register(TextComment)
+class TextCommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'body', 'post', 'created_on', 'approved')
+    list_filter = ('approved', 'created_on')
+    search_fields = ('name', 'email', 'body')
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
