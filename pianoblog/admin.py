3c1569270eaef84dc527b53f45720411d4f22f44
sonetto104 <peter_regan88@yahoo.com>
2023-06-24 16:56:35 +0000

    Add some summernote features.

--- pianoblog/admin.py
+++ pianoblog/admin.py
@@ -4,5 +4,7 @@ from django_summernote.admin import SummernoteModelAdmin
 
 @admin.register(TextPost)
 class TextPostAdmin(SummernoteModelAdmin):
-
+    
+    prepopulated_fields = {"slug": ("title",)}
+    list_filter = ("status", "created_on")
     summernote_fields = ('content')
