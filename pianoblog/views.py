db183ac7e9c3f70f35d1c1208a1f73553b499954
sonetto104 <peter_regan88@yahoo.com>
2023-07-06 09:36:49 +0000

    Alter conditional statements in base html so that they render properly in profile html. Take styling out of profile html and put into css file. Isolate profile image styling. Remove incorrectly placed and unneccesary bootstrap tags.

--- pianoblog/views.py
+++ pianoblog/views.py
@@ -102,14 +102,15 @@ class ProfileView(View):
         user = get_object_or_404(User, username=username)
         profile = get_object_or_404(Profile, owner=user)
         posts = Post.objects.filter(author=user).order_by('-created_on')[:5]
-         # Debug: Print post slugs
-        for post in posts:
-            print(post.slug)
         comments = PostComment.objects.filter(author=user)
+        
+        own_profile = False
+        if request.user.is_authenticated and request.user == user:
+            own_profile = True
         context = {
             'profile': profile,
             'posts': posts,
-            'own_profile': user == request.user,
+            'own_profile': own_profile,
             "user": user,
             "comments": comments,
         }
