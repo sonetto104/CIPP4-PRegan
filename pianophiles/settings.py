47f28b0e49521a5c5ead5140b7081b821d85489f
sonetto104 <peter_regan88@yahoo.com>
2023-07-10 06:19:11 +0000

    remove django heroku imports

--- pianophiles/settings.py
+++ pianophiles/settings.py
@@ -15,7 +15,6 @@ import os
 import dj_database_url
 if os.path.isfile('env.py'):
     import env
-import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -180,4 +179,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 #         'attachment_handler': 'pianoblog.handlers.cloudinary_handler',
 #     },
 # }
-django_heroku.settings(locals())
