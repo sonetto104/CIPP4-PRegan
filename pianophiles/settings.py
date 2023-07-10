76e9f5ce8627dcd8e9f1b8bccd59e1e15e26cdd0
sonetto104 <peter_regan88@yahoo.com>
2023-07-10 05:29:07 +0000

    Add different default storage variable.

--- pianophiles/settings.py
+++ pianophiles/settings.py
@@ -151,7 +151,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-SATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
+
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    }
+}
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
