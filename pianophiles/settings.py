507d8af037ff240bd3496266d02df6c349ca0ab4
sonetto104 <peter_regan88@yahoo.com>
2023-07-10 05:39:47 +0000

    Remove default storage variable

--- pianophiles/settings.py
+++ pianophiles/settings.py
@@ -161,7 +161,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
 CLOUDINARY_URL = os.environ.get('CLOUDINARY_URL')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 CLOUDINARY_UNSIGNED_UPLOAD_PRESET = os.environ.get('CLOUDINARY_UNSIGNED_UPLOAD_PRESET')
