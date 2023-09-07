11ea84eaa1d0c7431f1d0343d897e73e43743ba9
sonetto104 <peter_regan88@yahoo.com>
2023-09-07 12:50:54 +0000

    Tidy up forms.py file and add doc strings.

--- pianoblog/forms.py
+++ pianoblog/forms.py
@@ -1,17 +1,23 @@
-from .models import PostComment, Post, ImagePost, VideoPost, TextPost
+from .models import PostComment, Post, ImagePost, VideoPost, TextPost, Profile
 from django import forms
-from .models import Profile
 from allauth.account.forms import SignupForm
 from django.contrib.auth.models import User
 from django.utils.text import slugify
 
+
 class CommentForm(forms.ModelForm):
+    """
+    Create field/form where user can input comment.
+    """
     class Meta:
         model = PostComment
         fields = ('content',)
 
 
 class CustomSignupForm(SignupForm):
+    """
+    Create registration form with optional email field for new users.
+    """
     def clean_email(self):
         # Get the email value from the form
         email = self.cleaned_data.get('email')
@@ -32,24 +38,38 @@ class CustomSignupForm(SignupForm):
 
 
 class ProfileForm(forms.ModelForm):
+    """
+    Create form where user can update profile information. Limited to bio and
+    profile photo fields.
+    """
     class Meta:
         model = Profile
         fields = ['bio', 'profile_photo']
 
 
 class TextPostForm(forms.ModelForm):
+    """
+    Create the form where user will input information for text posts.
+    """
     class Meta:
         model = TextPost
         fields = ['title', 'content']
 
 
 class ImagePostForm(forms.ModelForm):
+    """
+    Create the form where user will input data for image posts.
+    """
     class Meta:
         model = ImagePost
         fields = ['title', 'image']
 
 
 class VideoPostForm(forms.ModelForm):
+    """
+    Create the form where user will input data for video posts.
+    """
     class Meta:
         model = VideoPost
         fields = ['title', 'video']
+        
