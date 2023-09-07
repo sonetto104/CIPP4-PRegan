796e07dfc63ee144256d4d9342c0c4ed750feb6a
sonetto104 <peter_regan88@yahoo.com>
2023-09-07 12:42:07 +0000

    Tidy up admin.py file and add doc strings

--- pianoblog/admin.py
+++ pianoblog/admin.py
@@ -5,12 +5,14 @@ from django import forms
 
 # admin.register block taken from Code Institute's Codestar Walkthrough Project
 
+
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
+    """Add Post model to admin panel"""
 
     def approve_posts(modeladmin, request, queryset):
         queryset.update(status=1)
- 
+
     list_display = ('title', 'slug', 'status', 'created_on')
     list_filter = ("status", "created_on")
     search_fields = ['title']
@@ -19,7 +21,8 @@ class PostAdmin(admin.ModelAdmin):
 
 @admin.register(TextPost)
 class TextPostAdmin(SummernoteModelAdmin):
-    
+    """Add TextPost model to admin panel"""
+
     list_display = ('title', 'slug', 'status', 'created_on')
     prepopulated_fields = {"slug": ("title",)}
     list_filter = ("status", "created_on")
@@ -29,6 +32,7 @@ class TextPostAdmin(SummernoteModelAdmin):
 
 @admin.register(ImagePost)
 class ImagePostAdmin(SummernoteModelAdmin):
+    """Register ImagePost model to admin panel"""
 
     list_display = ('title', 'slug', 'status', 'created_on')
     prepopulated_fields = {"slug": ("title",)}
@@ -38,18 +42,17 @@ class ImagePostAdmin(SummernoteModelAdmin):
 
 @admin.register(VideoPost)
 class VideoPostAdmin(SummernoteModelAdmin):
-
+    """Add VideoPost model to admin panel"""
     list_display = ('title', 'slug', 'status', 'created_on')
     prepopulated_fields = {"slug": ("title",)}
     list_filter = ("status", "created_on")
     search_fields = ['title', 'content']
 
 
-# admin.register block taken from Code Institute's Codestar Walkthrough Project
-
 @admin.register(PostComment)
 class PostCommentAdmin(admin.ModelAdmin):
-    
+    """Add PostComment model to admin panel"""
+
     list_display = ('author', 'content', 'post', 'created_on', 'approved')
     list_filter = ('approved', 'created_on')
     search_fields = ('author_username', 'email', 'content')
@@ -61,6 +64,7 @@ class PostCommentAdmin(admin.ModelAdmin):
 
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
+    """"Add Profile model to admin panel"""
 
     list_display = ('owner', 'created_on', 'status')
     list_filter = ('status', 'created_on')
@@ -70,24 +74,3 @@ class ProfileAdmin(admin.ModelAdmin):
 
     def approve_profile(self, request, queryset):
         queryset.update(approved=True)
-
-# @admin.register(ImagePostComment)
-# class ImageCommentAdmin(admin.ModelAdmin):
-#     list_display = ('author', 'content', 'imagepost', 'created_on', 'approved')
-#     list_filter = ('approved', 'created_on')
-#     search_fields = ('author__username', 'content')
-#     actions = ['approve_comments']
-
-#     def approve_comments(self, request, queryset):
-#         queryset.update(approved=True)
-
-
-# @admin.register(VideoPostComment)
-# class VideoCommentAdmin(admin.ModelAdmin):
-#     list_display = ('author', 'content', 'videopost', 'created_on', 'approved')
-#     list_filter = ('approved', 'created_on')
-#     search_fields = ('author__username', 'content')
-#     actions = ['approve_comments']
-
-#     def approve_comments(self, request, queryset):
-#         queryset.update(approved=True)
