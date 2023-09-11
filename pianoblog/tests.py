da23c47b51ba6168187b612bf74ea5fa4941280c
sonetto104 <peter_regan88@yahoo.com>
2023-09-11 10:42:57 +0000

    Create working test for image post model creation.

--- pianoblog/tests.py
+++ pianoblog/tests.py
@@ -28,5 +28,28 @@ class ModelCreationTest(TestCase):
         self.assertEqual(saved_text_post.content, 'This is a test text post content.')
         self.assertEqual(saved_text_post.status, 0)
         self.assertEqual(saved_text_post.author, self.user)
-    # Add similar tests for other models like PostComment and Profile
+
+    def test_create_image_post(self):
+        image_id = 'Keyboard_cat_ouvbg1' #Using 404 image stored in Cloudinary as test image
+
+        # Create an ImagePost instance with the Cloudinary ID
+        image_post = ImagePost.objects.create(
+            title='Test Image Post',
+            slug='test-image-post',
+            image=image_id,  # Set the Cloudinary ID
+            status=0,  # Assuming 0 is the default status
+            author=self.user,  # Set the author
+        )
+
+        # Retrieve the created ImagePost from the database
+        saved_image_post = ImagePost.objects.get(pk=image_post.pk)
+
+        # Assert that the retrieved ImagePost matches the created one
+        self.assertEqual(saved_image_post.title, 'Test Image Post')
+        self.assertEqual(saved_image_post.slug, 'test-image-post')
+
+        # Compare the image field with the expected public ID
+        self.assertEqual(saved_image_post.image.public_id, image_id)
+        self.assertEqual(saved_image_post.status, 0)
+        self.assertEqual(saved_image_post.author, self.user)
 
